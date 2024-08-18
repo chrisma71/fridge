cc9d6f6b4594773a5fb860e18d3320554a74ca91
chrisma71 <chris.pengfei.ma@gmail.com>
2024-08-18 08:12:04 -0400

    vercel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,28 +4,45 @@ import Landing from './pages/landing/Landing';
 import Goals from './pages/goals&info/goals';
 import NutritionTracker from './pages/nutrition/NutritionTracker';
 import Fridge from './pages/fridge/Fridge';
-import RecipeMaker from './pages/recipeMaker/recipeMaker'; // Import the RecipeMaker component
+import RecipeMaker from './pages/recipeMaker/recipeMaker';
 import Cookies from 'js-cookie';
 
 const App: React.FC = () => {
   useEffect(() => {
     const fetchUserId = async () => {
       let userId = Cookies.get('userId');
-      if (!userId) {
-        const response = await fetch('http://localhost:5000/api/user-id', {
-          method: 'GET',
-        });
-        const data = await response.json();
-        userId = data.userId;
-        Cookies.set('userId', userId, { expires: 365 }); // Expires in 1 year
 
-        await fetch(`http://localhost:5000/api/users/${userId}/create`, {
-          method: 'POST',
-        });
+      if (!userId) {
+        try {
+          const response = await fetch('http://localhost:5000/api/user-id', {
+            method: 'GET',
+          });
+
+          if (!response.ok) {
+            throw new Error('Failed to fetch user ID');
+          }
+
+          const data = await response.json();
+          userId = data.userId;
+
+          if (userId) {
+            Cookies.set('userId', userId, { expires: 365 }); // Expires in 1 year
+
+            await fetch(`http://localhost:5000/api/users/${userId}/create`, {
+              method: 'POST',
+            });
+          }
+        } catch (error) {
+          console.error('Error fetching or creating user:', error);
+        }
       } else {
-        await fetch(`http://localhost:5000/api/users/${userId}/create`, {
-          method: 'POST',
-        });
+        try {
+          await fetch(`http://localhost:5000/api/users/${userId}/create`, {
+            method: 'POST',
+          });
+        } catch (error) {
+          console.error('Error creating user:', error);
+        }
       }
     };
 
@@ -40,7 +57,7 @@ const App: React.FC = () => {
           <Route path="/a/goals" element={<Goals />} />
           <Route path="/a/tracker" element={<NutritionTracker />} />
           <Route path="/a/fridge" element={<Fridge />} />
-          <Route path="/a/meal-planner" element={<RecipeMaker />} /> {/* Add the RecipeMaker route */}
+          <Route path="/a/meal-planner" element={<RecipeMaker />} />
         </Routes>
       </div>
     </Router>
