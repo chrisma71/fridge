c9c551c6b6d2794fa89d6566fbca9342f558e72c
chrisma71 <chris.pengfei.ma@gmail.com>
2024-08-20 13:44:19 -0400

    vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure this is 'dist'
+    outDir: 'dist',  // Ensure this is set to 'dist'
   },
 });
